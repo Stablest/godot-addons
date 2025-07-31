f8eac62514cadab4adf1bd2fa1a4998b4ef471ee
Stablest <marlonlocci@hotmail.com>
2025-07-31 20:58:01 -0300

    feat: created TimeCycle addon

---
+++ addons/TimeCycle.cs
@@ -0,0 +1,80 @@
+using Godot;
+
+namespace Addons.addons;
+
+[GlobalClass]
+public partial class TimeCycle : Node
+{
+    [Signal]
+    public delegate void TimeChangedEventHandler(uint currentTime);
+
+    [Signal]
+    public delegate void CycleEndedEventHandler();
+
+    [ExportGroup("Speed properties")]
+    [Export] public uint TimeSpeed { get; private set; } = 1;
+    [Export] public uint TickIntervalInSeconds { get; private set; } = 1;
+    [ExportGroup("Cycle properties")]
+    [Export] public uint CycleDurationInSeconds { get; private set; }
+    [Export] public uint DaytimeStartInSeconds { get; private set; }
+    [Export] public uint DaytimeEndInSeconds { get; private set; }
+    [Export] public bool ShouldFreezeWhenEnded { get; private set; }
+    public uint CurrentTime { get; private set; }
+    private Timer _timer;
+    private bool _wasDaytime;
+
+    public override void _Ready()
+    {
+        _timer = new Timer
+        {
+            WaitTime = TickIntervalInSeconds,
+            Autostart = true,
+            OneShot = false,
+        };
+        AddChild(_timer);
+        CurrentTime = 0;
+        _timer.Timeout += OnTimerTick;
+    }
+
+    private void Validate()
+    {
+        if (DaytimeEndInSeconds <= DaytimeStartInSeconds)
+        {
+            GD.PushError(this + " is misconfigured: DaytimeStartInSeconds must be greater than DaytimeEndInSeconds.");
+        }
+    }
+
+    private void OnTimerTick()
+    {
+        CurrentTime += TimeSpeed * TickIntervalInSeconds;
+        if (CurrentTime >= CycleDurationInSeconds)
+        {
+            if (ShouldFreezeWhenEnded)
+            {
+                CurrentTime = CycleDurationInSeconds;
+            }
+            else
+            {
+                CurrentTime -= CycleDurationInSeconds;
+            }
+            EmitSignalCycleEnded();
+        }
+
+        EmitSignal(SignalName.TimeChanged, CurrentTime);
+    }
+
+    private bool IsDaytime()
+    {
+        return CurrentTime >= DaytimeStartInSeconds && CurrentTime < DaytimeEndInSeconds;
+    }
+
+    public void Freeze()
+    {
+        SetProcess(false);
+    }
+
+    public void Unfreeze()
+    {
+        SetProcess(true);
+    }
+}
